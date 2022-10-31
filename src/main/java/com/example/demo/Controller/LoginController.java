96032406c1a1cb2d1982fec04b5947cbbe65d9d9
pssaxena <41469980+pssaxena@users.noreply.github.com>
2022-10-31 18:54:12 +0530

    Update LoginController.java

--- src/main/java/com/example/demo/Controller/LoginController.java
+++ src/main/java/com/example/demo/Controller/LoginController.java
@@ -1,14 +1,14 @@
 package com.example.demo.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping("/login")
-    @ResponseBody
+	@RequestMapping(value = "/login")
+    
 	public String loginMessage(){
-		return "Hello World modified";
+		return "login";
 	}
 }
